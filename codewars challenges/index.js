991cf0d4e5b9c65508695b7e60fd69d14d855b22
tanvirch0750 <tanvir0750@yahoo.com>
2020-06-19 00:42:41 +0600

    added three new challenge

--- codewars challenges/index.js
+++ codewars challenges/index.js
@@ -1,4 +1,4 @@
-// todo Replace With Alphabet Position
+// todo -> (1) Replace With Alphabet Position
 {
   function alphabetPosition(text) {
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -14,24 +14,21 @@
       }
     }
 
-    return alphabetNumber.join(" ");;
-
+    return alphabetNumber.join(" ");
   }
 
-  const hello = (alphabetPosition('fghij'));
+  const hello = alphabetPosition("fghij");
   console.log(hello);
-  console.log(typeof (hello))
-
+  console.log(typeof hello);
 }
 
-
-// todo -> Find The Parity Outlier
+// todo -> (2) Find The Parity Outlier
 {
   function findOutlier(integers) {
-    const evenArr = integers.filter(value => {
+    const evenArr = integers.filter((value) => {
       return value % 2 === 0;
     });
-    const oddArr = integers.filter(value => {
+    const oddArr = integers.filter((value) => {
       return value % 2 !== 0;
     });
 
@@ -41,5 +38,64 @@
       return oddArr[0];
     }
   }
-  findOutlier([1, 2, 3])
+  findOutlier([1, 2, 3]);
+}
+
+// todo -> (3) Find the first non-consecutive number
+{
+  function firstNonConsecutive(arr) {
+    for (i = 0; i < arr.length - 1; i++) {
+      const cur = arr[i];
+      const next = arr[i + 1];
+      if (cur + 1 !== next) {
+        return next;
+      }
+    }
+
+    return null;
+  }
+  const res = firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]);
+  console.log(res);
+
+  // * Or we can do that also
+  {
+    function firstNonConsecutive(arr) {
+      let result = arr.find((val, index) => val !== index + arr[0]);
+
+      return Number.isInteger(result) ? result : null;
+    }
+  }
+}
+
+// todo -> (4) No oddities here
+{
+  function noOdds(values) {
+    // Return all non-odd values
+    return values.filter((value) => value % 2 === 0);
+  }
+  const res = noOdds([1, 2, 3, 4, 5, 6, 7]);
+  console.log(res);
+}
+
+// todo -> (5) Partial Keys
+{
+  function partialKeys(obj) {
+    // loop over the properties of the object
+    // create all substring properties of the object and assign them to value
+
+    const newObj = {};
+    Object.keys(obj)
+      .sort()
+      .reverse()
+      .forEach((key) => {
+        for (i = 0; i < key.length; i++) {
+          const newKey = key.substring(0, i + 1);
+          newObj[newKey] = obj[key];
+        }
+      });
+
+    return newObj;
+  }
+
+  partialKeys({ abcd: 1 });
 }
